5e4abc4e7aa7dbb7d2bb900d1359963966d96fe2
yellowfox-star-is <yellowfox.star.is@gmail.com>
2021-11-01 19:57:32 +0100

    Bit manipulation header

---
+++ bit.h
@@ -0,0 +1,4 @@
+#define BIT_SET(target, position) target |= 1 << position
+#define BIT_CLR(target, position) target &= ~(1 << position)
+#define BIT_FLP(target, position) target ^= 1 << position
+#define BIT_GET(target, position) (target & (1 << position)) >> position
