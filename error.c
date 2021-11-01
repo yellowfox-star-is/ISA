a1bd9fb1dba6ec9ca4f3bda71e5e5904361eddab
yellowfox-star-is <yellowfox.star.is@gmail.com>
2021-11-01 19:58:17 +0100

    Taken error module from another project

---
+++ error.c
@@ -0,0 +1,40 @@
+// error.c
+// Řešení IJC-DU1, příklad b), 19.3.2020
+// Autor: Václav Sysel, FIT
+// Přeloženo: gcc 7.5.0
+//
+// využito k projektu ISA
+// drobné úpravy provedeny
+// error.c version 2
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "error.h"
+
+#define ERR_OUT stderr
+
+void warning_msg(const char *fmt, ...)
+{
+
+    va_list args;
+
+    va_start(args, fmt);
+    fprintf(ERR_OUT, "WARNING: ");
+    vfprintf(ERR_OUT, fmt, args);
+    va_end(args);
+}
+
+void error_exit(int exit_code, const char *fmt, ...)
+{
+
+    va_list args;
+
+    va_start(args, fmt);
+    fprintf(ERR_OUT, "ERROR: ");
+    vfprintf(ERR_OUT, fmt, args);
+    va_end(args);
+
+    exit(exit_code);
+}
---
+++ error.h
@@ -0,0 +1,17 @@
+// error.h
+// Řešení IJC-DU1, příklad b), 19.3.2020
+// Autor: Václav Sysel, FIT
+// Přeloženo: gcc 7.5.0
+//
+// využito k projektu ISA
+// drobné úpravy provedeny
+// error.h version 2
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stdlib.h>
+void warning_msg(const char *fmt, ...);
+void error_exit(int exit_code, const char *fmt, ...);
+
+#endif //ERROR_H
