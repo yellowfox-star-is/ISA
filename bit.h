12ff830ebaa47fd343f3f2b2ff4b17b311944f48
yellowfox <yellowfox.star.is@gmail.com>
2021-11-02 18:20:57 +0100

    Added bool to string conversion.

--- bit.h
+++ bit.h
@@ -2,3 +2,4 @@
 #define BIT_CLR(target, position) target &= ~(1 << position)
 #define BIT_FLP(target, position) target ^= 1 << position
 #define BIT_GET(target, position) (target & (1 << position)) >> position
+#define BOOL2STRING(input) (input ? "true" : "false")
